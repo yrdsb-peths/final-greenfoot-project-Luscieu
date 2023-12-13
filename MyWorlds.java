882bf68303bc143020fa4fa17dbaf1dcc5260c48
Luscieu <341043313@gapps.yrdsb.ca>
2023-12-13 08:40:29 -0500

    In process of implementing "Physics" codes by MatheMagician

---
+++ MyWorlds.java
@@ -0,0 +1,50 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorlds extends World
+{
+    Wood wood;
+    Ball ball;
+    /**
+     * Constructor for objects of class MyWorlds.
+     * 
+     */
+    public MyWorlds()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1);
+        
+    }
+    public void act()
+    {
+        if(Greenfoot.mouseClicked(null)){
+            MouseInfo mouse = Greenfoot.getMouseInfo();
+            if(mouse.getButton() <2)
+            {
+                removeObject(ball);
+                ball = new Ball();
+                addObject(ball,100,275);
+                if(275-mouse.getY() > -50)
+                    ball.velocityY = (275-mouse.getY())/10;
+                else
+                    ball.velocityY =-5;
+                if(100-mouse.getX() < 50)
+                    ball.velocityX = (100-mouse.getX())/10;
+                else
+                    ball.velocityX = 5;
+            }
+            else
+            {
+                wood = new Wood(20);
+                addObject(wood, mouse.getX(),mouse.getY());
+            }
+        }
+        if(Greenfoot.isKeyDown("right"))
+            wood.setRotation(wood.getRotation()+1);
+    }
+}
---
+++ Pig.java
@@ -0,0 +1,111 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Pig here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Pig extends Animate
+{
+    float velocityY = 0;
+    float velocityX = 0;
+    int rSpeed = 0;
+    /**
+     * Act - do whatever the Wood wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Pig() 
+    {
+        getImage().setColor(Color.BLUE);
+        getImage().fillRect(0,0,20,20);
+    }    
+    public void act()
+    {
+        setRotation(getRotation()+rSpeed);
+        setLocation(getX()+(int)Math.round(velocityX),getY());
+        if(getY() >= 287)
+            velocityX = velocityX*.7f;
+        if(velocityY == 0 && getRotation() > 0)
+        {
+            rSpeed = 0;
+            setRotation(0);
+        }
+        if(velocityY == 0)
+        {
+            setRotation(Math.abs(getRotation())-1);
+        }
+        impartForce();
+        {
+            velocityY += .1;
+            //speed += .1;
+        }
+        if(getY() <= 288-(10*Math.sin(Math.abs(Math.toRadians(getRotation()))))&&!getBelow())
+            setLocation(getX(),getY()+(int)Math.round(velocityY));
+        else{
+            if(velocityY>1.5){
+                getWorld().removeObject(this);
+                return;
+            }
+            velocityY = 0;
+            //velocityX = velocityX*.7f;
+            //velocityX = 0;
+        }
+    }
+    public void impartForce()
+    {
+        List<Animate> targets = getIntersectingObjects(Animate.class);
+        for(int i = 0;i<targets.size();i++){
+            if(targets.get(i).getX()>getX()){
+                float whatever =  (targets.get(i)).getX()/(targets.get(i)).getY();
+                float newX = velocityX*Math.abs(1-whatever/(velocityX/velocityY));
+                float newY = velocityY*Math.abs(1-whatever/(velocityX/velocityY));
+                (targets.get(i)).transferVelocity(velocityX/2, velocityY/2);
+                velocityY = velocityY/2;//-newY;
+                velocityX = velocityX/2;//2-newX;
+            }
+            //else if(targets.get(0).getY()>getY()&&velocityY >.5)
+            {}
+        }
+    }
+    public boolean getBelow()
+    {
+        List<Animate> bottom= getWorld().getObjects(Animate.class);
+        boolean verdict = false;
+        boolean flip = false;
+        Actor flipper = bottom.get(0);
+        for(int i = 0;i<bottom.size();i++)
+        {
+            if((bottom.get(i)).getY() > getY()&&(bottom.get(i)).getY() < getY()+16&&(bottom.get(i)).getX() > getX()-5&&(bottom.get(i)).getX() < getX()+5)
+            {
+                verdict = true;
+                setLocation(getX(),(bottom.get(i)).getY()-15);
+            }
+            else if((bottom.get(i)).getY() > getY()&&(bottom.get(i)).getY() < getY()+16&&(bottom.get(i)).getX() > getX()-10&&(bottom.get(i)).getX() < getX()+10)
+            {
+                flip = true;
+                flipper = bottom.get(i);
+            }
+        }
+        /*if(verdict == false && flip == true)
+        {
+            int dif = flipper.getX()-getX();
+            rSpeed = dif/2;
+            velocityX +=dif/10;
+        }*/
+        return verdict;
+    }
+    public void transferVelocity(float x, float y)
+    {
+        velocityX += x;
+        velocityY += y;
+        stop();
+    }
+    public void stop()
+    {
+        if(Math.pow(Math.pow(velocityY,2)+Math.pow(velocityX,2),.5)>1.5){
+            getWorld().removeObject(this);
+            return;
+        }
+    }
+}
